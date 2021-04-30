f4b5d8f64169e16d56ee5fa755b25a857f9b694f
Dareka <42326650+DarekaSama@users.noreply.github.com>
2021-04-30 19:33:57 +0200

    Create student.module.ts

---
+++ src/app/student/student.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { StudentRoutingModule } from './student-routing.module';
+import { StudentComponent } from './student.component';
+import { SharedModule } from '../shared/shared.module';
+import { StudentListComponent } from './pages/student-list/student-list.component';
+import { StudentFormComponent } from './pages/student-form/student-form.component';
+
+
+@NgModule({
+  declarations: [StudentComponent, StudentListComponent, StudentFormComponent],
+  imports: [
+    CommonModule, 
+    StudentRoutingModule,
+    SharedModule,
+  ]
+})
+export class StudentModule { }
